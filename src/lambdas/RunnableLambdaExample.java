9f07065890a7e2ac6c93de67e261314fb4712805
Simon Vargas <sjvargas01@gmail.com>
2024-07-30 18:50:16 -0400

    first commit

---
+++ src/lambdas/RunnableLambdaExample.java
@@ -0,0 +1,26 @@
+package lambdas;
+
+public class RunnableLambdaExample {
+
+    public static void main(String[] args) {
+
+        /**
+         * prior to java 8
+         */
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Inside Runnable 1");
+
+            }
+        };
+
+        new Thread(runnable).start();
+
+        // java 8 lambda
+        // () -> {};
+        Runnable runnable1 = () -> System.out.println("Inside Runnable 2");
+        new Thread(runnable1).start();
+
+    }
+}
